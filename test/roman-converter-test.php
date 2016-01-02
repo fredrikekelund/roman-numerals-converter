cd8cbba446bf69e83b7f15a2ecc4c3c1b06352fd
Fredrik Ekelund <bohemenian@gmail.com>
2016-01-02 21:52:39 +0100

    Changed coding style to spaces, 2 in width
    Enforcing that style with a .editorconfig

--- test/roman-converter-test.php
+++ test/roman-converter-test.php
@@ -3,24 +3,24 @@ require 'roman-converter.php';
 
 class RomanConverterTest extends PHPUnit_Framework_TestCase
 {
-	public function testToInteger()
-	{
-		$romanConverter = new RomanConverter();
+  public function testToInteger()
+  {
+    $romanConverter = new RomanConverter();
 
-		$this->assertEquals(1, $romanConverter->toInteger('I'));
-		$this->assertEquals(2, $romanConverter->toInteger('II'));
-		$this->assertEquals(4, $romanConverter->toInteger('IV'));
-		$this->assertEquals(472, $romanConverter->toInteger('CDLXXII'));
-		$this->assertEquals(1516, $romanConverter->toInteger('MDXVI'));
-		$this->assertEquals(2999, $romanConverter->toInteger('MMCMXCIX'));
-	}
+    $this->assertEquals(1, $romanConverter->toInteger('I'));
+    $this->assertEquals(2, $romanConverter->toInteger('II'));
+    $this->assertEquals(4, $romanConverter->toInteger('IV'));
+    $this->assertEquals(472, $romanConverter->toInteger('CDLXXII'));
+    $this->assertEquals(1516, $romanConverter->toInteger('MDXVI'));
+    $this->assertEquals(2999, $romanConverter->toInteger('MMCMXCIX'));
+  }
 
-	/**
-	 * @expectedException Exception
-	 */
-	public function testException()
-	{
-		$romanConverter = new RomanConverter();
-		$romanConverter->toInteger('CAB');
-	}
+  /**
+   * @expectedException Exception
+   */
+  public function testException()
+  {
+    $romanConverter = new RomanConverter();
+    $romanConverter->toInteger('CAB');
+  }
 }
